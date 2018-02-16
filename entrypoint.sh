689c6c49ed66a29c0b15cd0a9b311f22dacaf09d
Walid MNASRI <wmnasri@smart-team.tn>
2018-02-16 14:37:41 +0100

    change directory of nginx config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp crossover-api.conf /etc/nginx/sites-available/
+cp config/nginx/crossover-api.conf /etc/nginx/sites-available/
 
 ln -s /etc/nginx/sites-available/crossover-api.conf /etc/nginx/sites-enabled/crossover-api
 rm /etc/nginx/sites-enabled/default
